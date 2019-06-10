7fd7b18c33f2918676baa3d71be0627588f1b0cf
lee-simfy <leeb@simfyafrica.com>
2019-06-10 14:26:31 +0200

    Build out setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+      name='simfy_health',
+      version='0.0.1',
+      description='Simfy Africa Health',
+      url='https://github.com/simfy-africa/health.git',
+      author='Simfy Africa',
+      author_email='info@simfyafrica.com',
+      packages=find_packages(),
+      install_requires=['flask'],
+      zip_safe=False
+)
