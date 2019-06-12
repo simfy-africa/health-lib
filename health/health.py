d9d0eebf0bbf762b2058f3dac8acc4e1c8100923
lee-simfy <leeb@simfyafrica.com>
2019-06-12 12:31:38 +0200

    Add tests

--- health/health.py
+++ health/health.py
@@ -5,19 +5,20 @@ from flask import jsonify
 from flask.views import MethodView
 
 
-def git_tag_currently_checked_out():
+def _git_tag_currently_checked_out():
     checked_out_tag_cmd = 'git name-rev --tags --name-only $(git rev-parse HEAD)'
     # Shell must be True to perform command subsitution
-    tag = subprocess.run(checked_out_tag_cmd, shell=True, stdout=subprocess.PIPE)
-    tag = tag.stdout.decode('utf-8')
+    tag = subprocess.run(checked_out_tag_cmd, shell=True, stdout=subprocess.PIPE).stdout
+    tag = tag.decode('utf-8')
     return tag.strip()  # Comes with a trailing '\n'
 
 
 class APIHealthResource(MethodView):
-    health_data = {
-        'api_version': git_tag_currently_checked_out(),
-        'python_version': python_version(),
-    }
+    def __init__(self):
+        self.health_data = {
+            'api_version': _git_tag_currently_checked_out(),
+            'python_version': python_version(),
+        }
 
     def get(self):
         return jsonify(self.health_data)
